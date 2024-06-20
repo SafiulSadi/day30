0435d9ccfad825d0233cf4e92b1ed2e75a3ca0ec
A. S. M. Safiul Sadi <41629377+SafiulSadi@users.noreply.github.com>
2024-06-20 20:29:45 +0600

    initial

---
+++ main.py
@@ -0,0 +1,30 @@
+
+# # KeyError
+# a_dictionary = {"key":"value"}
+# value = a_dictionary["non_existant_key"]
+
+# # IndexError
+# fruit_list = ["Apple", "Banana", "Pear"]
+# fruit_list[3]
+
+# # TypeError
+# a = "abc"
+# print(a + 5)
+
+# FileNotFound
+try:
+    file = open("a_file.txt")
+    a_dictionary = {"key":"value"}
+    print(a_dictionary["snadf"])
+except FileNotFoundError:
+    file = open("a_file.txt", "w")
+    file.write("Something")
+except KeyError as error_message:
+    print(f"that key {error_message} does not exist")
+else:
+    content = file.read()
+    print(content)
+finally:
+    file.close()
+
+
